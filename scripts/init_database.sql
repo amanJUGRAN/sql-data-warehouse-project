d3e2b463c57ecdf2d9fe983a7979844216303d51
Aman Jugran <41915645+amanJUGRAN@users.noreply.github.com>
2025-09-27 01:11:36 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,40 @@
+/*
 
+-----------------------------------------------
+Use this script to create databases and schemas
+-----------------------------------------------
+
+*/
+
+
+
+use master 
+GO
+
+----------------------Checking if the Database we want to create already exists or not. If it exists then delete it.
+  
+IF EXISTS 
+(
+	select name from sys.databases where name = 'DataWarehouse'	
+)
+
+BEGIN
+	Drop database DataWarehouse
+END
+
+-------------------------Creating New Databse
+  
+create database DataWarehouse
+ 
+
+-------------------------Creating New Schemas  
+use DataWarehouse
+GO
+
+
+create schema bronze
+go
+create schema silver
+go
+create schema gold
+go
